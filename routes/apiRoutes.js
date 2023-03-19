315eee1a19812e69450fd6e988a31e8ff02cfdd3
Manuel Leung Chen <manuel.leungchen@gmail.com>
2023-03-19 17:43:47 -0400

    Updated api routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,27 +1,10 @@
 const router = require("express").Router();
-const db = require("../models");
+const booksController = require("../controllers/booksController");
 
+router.get("/books", booksController.findAll)
 
-router.get("/books", (req, res) => {
-    db.Book
-        .find(req.query)
-        .sort({ date: -1 })
-        .then(dbModel => res.json(dbModel))
-        .catch(err => res.status(422).json(err));
-})
+router.post("/books", booksController.create)
 
-router.post("/books", (req, res) => {
-    db.Book.create(req.body)
-        .then(dbModel => res.json(dbModel))
-        .catch(err => res.status(422).json(err));
-})
-
-router.delete("/books/:id", (req, res) => {
-    db.Book
-        .findById({ _id: req.params.id })
-        .then(dbModel => dbModel.remove())
-        .then(dbModel => res.json(dbModel))
-        .catch(err => res.status(422).json(err));
-})
+router.delete("/books/:id", booksController.remove)
 
 module.exports = router;
